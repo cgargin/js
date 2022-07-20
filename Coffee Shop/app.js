65588739a01fdd30a581fca4503685aa9af08642
GÖRKEM ALMAN <43208742+trgorkem44@users.noreply.github.com>
2022-07-20 15:55:26 +0300

    Coffee Shop
    Html Css Js.

---
+++ Coffee Shop/app.js
@@ -0,0 +1,40 @@
+let navbar = document.querySelector(".navbar");
+
+document.querySelector("#menu-btn").onclick = () => {
+  navbar.classList.toggle("active");
+  seacrhForm.classList.remove("active");
+  cartItem.classList.remove("active");
+};
+
+let seacrhForm = document.querySelector(".search-form");
+
+document.querySelector("#search-btn").onclick = () => {
+  seacrhForm.classList.toggle("active");
+  navbar.classList.remove("active");
+  cartItem.classList.remove("active");
+};
+
+let cartItem = document.querySelector(".cart-items-container");
+
+document.querySelector("#cart-btn").onclick = () => {
+  cartItem.classList.toggle("active");
+  navbar.classList.remove("active");
+  seacrhForm.classList.remove("active");
+};
+
+document.querySelector("#search-btn").onclick = () => {
+  seacrhForm.classList.toggle("active");
+  navbar.classList.remove("active");
+  cartItem.classList.remove("active");
+};
+
+window.onscroll = () => {
+  navbar.classList.remove("active");
+  seacrhForm.classList.remove("active");
+  cartItem.classList.remove("active");
+};
+
+
+//////////////////////////////////////About US
+
+
